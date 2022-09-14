df576156b7365d171660374d8cf1046dfce26594
simjeongsoo <simking95@naver.com>
2022-09-14 18:25:26 +0900

    실습 1-8 for문

---
+++ Java/01.basic/01-2.Iterate/src/SumFor.java
@@ -0,0 +1,21 @@
+// 실습 1-8
+// for 문으로 1,2,...,n 의 합을 구함
+
+import java.util.Scanner;
+
+public class SumFor {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("1부터 n 까지의 합을 구합니다.");
+        System.out.print("n 값 : ");
+        int n = stdIn.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+
+    }
+}
