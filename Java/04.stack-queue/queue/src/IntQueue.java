e6b1b04699f6c6a72b682badf2e540c55f0a12cc
simjeongsoo <simking95@naver.com>
2022-10-05 14:16:59 +0900

    [링버퍼로 큐 구현] - 필드, 예외메서드, 생성자 구현

---
+++ Java/04.stack-queue/queue/src/IntQueue.java
@@ -0,0 +1,31 @@
+// 실습 4-3A
+// 링버퍼로 큐 만들기
+// int형 고정 길이 큐
+
+public class IntQueue {
+    private int[] que;      // 큐용 배열
+    private int capacity;   // 큐의 용량
+    private int front;      // 맨 앞의 요소 커서
+    private int rear;       // 맨 뒤의 요소 커서
+    private int num;        // 현재 데이터 개수
+
+    public class EmptyIntQueueException extends RuntimeException {
+        public EmptyIntQueueException() {
+        }
+    }
+
+    public class OverflowIntQueueException extends RuntimeException {
+        public OverflowIntQueueException() {
+        }
+    }
+
+    public IntQueue(int maxlen) {
+        num = front = rear = 0;
+        capacity = maxlen;
+        try {
+            que = new int[capacity];
+        } catch (OutOfMemoryError e) {
+            capacity = 0;
+        }
+    }
+}
