b61c23d35f489459800a197aaa39ee8c213bccb4
simjeongsoo <simking95@naver.com>
2022-10-21 13:04:10 +0900

    [8-Queen Problem] - 8 Queen Problem solution

---
+++ Java/_8-QueenProblem/src/EightQueen.java
@@ -0,0 +1,45 @@
+// 8 Queen Problem
+
+public class EightQueen {
+
+    static boolean[] flag_a = new boolean[8];               // 각 행에 퀸을 배치했는지 체크
+    static boolean[] flag_b = new boolean[15];              // '/' 대각선 방향으로 퀸을 배치했는지 체크
+    static boolean[] flag_c = new boolean[15];              // '\' 대각선 방향으로 퀸을 배치했는지 체크
+    static int[] pos = new int[8];                          // 각 열에 있는 퀸의 위치
+    static int cnt = 0;
+
+    static void print() {
+        //--각 열에 있는 퀸의 위치를 출력--//
+        ++cnt;
+        for (int i = 0; i < 8; i++) {
+            System.out.printf("%2d", pos[i]);
+        }
+        System.out.println();
+    }
+
+    static void set(int i) {
+        //--i 열에 알맞은 위치에 퀸을 배치--//
+        for (int j = 0; j < 8; j++) {
+            if (!flag_a[j] &&                   // 가로(j행)에 아직 배치하지 않음
+                    !flag_b[i + j] &&           // '/' 대각선에 아직 배치하지 않음
+                    !flag_c[i - j + 7]) {       // '\' 대각선에 아직 배치하지 않음
+                pos[i] = j;                     // 퀸을 j행에 배치
+                if (i == 7) {                   // 모든 열에 배치
+                    print();
+                } else {
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
+                    set(i + 1);
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        set(0);
+        System.out.println(cnt);
+
+    }
+
+}
